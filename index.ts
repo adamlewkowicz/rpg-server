4ed58b192ca200e284a286368ec2ac1c824900cc
alk831 <adam54111@gmail.com>
2019-06-03 16:26:35 +0200

    move database seed to helpers

--- index.ts
+++ index.ts
@@ -1,46 +1,23 @@
 import express from 'express';
 import * as http from 'http';
 import { sequelize } from './src/db';
-import { Character } from './src/models/Character';
-import { Location } from './src/models/Location';
-import { ItemLoot, ItemLocation, ItemType } from './src/models/Item';
 import mainController from './src/controllers/main';
+import { seedDatabase } from './src/helpers';
 
 const app = express();
 const server = new http.Server(app);
 const io = require('socket.io')(http);
 
-app.get('/', function(req: any, res: any){
+app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 let force = false;
 // force = true;
 sequelize.sync({ force })
-  .then(async (): Promise<any> => {
+  .then((): Promise<any> | void => {
     if (force) {
-      await Promise.all([
-        Character.createWithLocation({ name: 'Razuglag' }),
-        Character.createWithLocation({ name: 'Roo' }),
-        Character.createWithLocation({ name: 'Booom', locationId: 2 }),
-        Character.createWithLocation({ name: 'Test', locationId: 2 }),
-        Character.createWithLocation({ name: 'Ratatatat' }),
-        Location.create({ name: 'Novigrad' }),
-        Location.create({ name: 'Yar' }),
-        ItemType.create({ name: 'Weeper' }),
-        ItemType.create({ name: 'Zireael' }),
-      ])
-      await Promise.all([
-        ItemLoot.create({ lootedBy: 1, typeId: 1 }),
-        ItemLoot.create({ lootedBy: 1, typeId: 2 }),
-        ItemLoot.create({ lootedBy: 2, typeId: 2 })
-      ])
-      return Promise.all([
-        ItemLocation.create({ charId: 1, lootId: 1, position: 1 }),
-        ItemLocation.create({ charId: 1, lootId: 1, position: 2 }),
-        ItemLocation.create({ charId: 1, lootId: 2, position: 3 }),
-        ItemLocation.create({ charId: 2, lootId: 2, position: 1 })
-      ]);
+      return seedDatabase();
     }
   })
   .then(() => console.log('synced'))
--- src/helpers.ts
+++ src/helpers.ts
@@ -2,7 +2,7 @@ import { Location } from './models/Location';
 import { CharacterLocation } from './models/CharacterLocation';
 import { Character } from './models/Character';
 import * as Objects from 'rpg-shared/lib/objects';
-import { ItemLocation } from './models/Item';
+import { ItemLocation, ItemType, ItemLoot } from './models/Item';
 
 export async function initGame(clientId: number) {
   const character = await Character.findByPk(clientId);
@@ -52,4 +52,29 @@ export async function getDataForNextLocation(nextLocationId: number, charId: num
   }
 
   return { nextLocation, characters }
+}
+
+export async function seedDatabase() {
+  await Promise.all([
+    Character.createWithLocation({ name: 'R4azurw' }),
+    Character.createWithLocation({ name: 'Roo' }),
+    Character.createWithLocation({ name: 'Rjwk', locationId: 2 }),
+    Character.createWithLocation({ name: 'Rmia', locationId: 2 }),
+    Character.createWithLocation({ name: 'Jwnejk' }),
+    Location.create({ name: 'Novigrad' }),
+    Location.create({ name: 'Yar' }),
+    ItemType.create({ name: 'Weeper' }),
+    ItemType.create({ name: 'Zireael' }),
+  ])
+  await Promise.all([
+    ItemLoot.create({ lootedBy: 1, typeId: 1 }),
+    ItemLoot.create({ lootedBy: 1, typeId: 2 }),
+    ItemLoot.create({ lootedBy: 2, typeId: 2 })
+  ])
+  return Promise.all([
+    ItemLocation.create({ charId: 1, lootId: 1, position: 1 }),
+    ItemLocation.create({ charId: 1, lootId: 1, position: 2 }),
+    ItemLocation.create({ charId: 1, lootId: 2, position: 3 }),
+    ItemLocation.create({ charId: 2, lootId: 2, position: 1 })
+  ]);
 }
